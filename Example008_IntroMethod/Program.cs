c9d06b6aef01b57d4ef0a609b90b9ffe3accaeb7
FeNiksleks <feniksleks@yandex.ru>
2023-04-24 19:17:47 +0300

    008

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -15,10 +15,10 @@ int a3 = 7;
 int b3 = 34;
 int c3 = 98;
 
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 
 
 Console.WriteLine(max);
